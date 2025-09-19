fea46caa18624730a04be4cfd4d4bbb3afee0c05
Alisson Curvina <alisson.curvina@gmail.com>
2025-09-19 20:42:33 -0300

    feat(p1): cria componente principal e faz hello

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+const App = () => {
+  return(
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Hello, Lembretes</h1>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
+
+export default App
