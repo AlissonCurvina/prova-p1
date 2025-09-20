b924f5a687830d0c02a09d376b254a7d927fa54e
Alisson Curvina <alisson.curvina@gmail.com>
2025-09-19 21:44:27 -0300

    feat(p1): exibe lista ficticia

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import LembreteEntrada from './LembreteEntrada'
+import LembreteLista from './LembreteLista'
 
 const App = () => {
   return(
     <div className="container">
+      <div className="row mt-4">
+        <div className="col">
+          <LembreteLista/>
+        </div>
+      </div>
       <div className="row mt-4">
         <div className="col">
           <LembreteEntrada/>
---
+++ src/LembreteLista.jsx
@@ -0,0 +1,12 @@
+const LembreteLista = (props) => {
+
+    return (
+    <div>
+        <div>Preparar aula de programação</div>
+        <div>Fazer feira</div>
+        <div>Preparar marmitas</div>
+    </div>
+    )
+}
+
+export default LembreteLista
